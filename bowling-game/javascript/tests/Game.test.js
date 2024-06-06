fd9e28f9830a9d6b47cff215dbb42b2725f5d925
Brian Dady <bndady@gmail.com>
2024-06-06 13:50:05 -0500

    test that Game.roll method exists

---
+++ bowling-game/javascript/tests/Game.test.js
@@ -0,0 +1,9 @@
+describe("Game should", () => {
+  const Game = require("../Game");
+
+  test('have a method named "roll"', () => {
+    const game = new Game();
+
+    expect(game.roll).toBeInstanceOf(Function);
+  });
+});
