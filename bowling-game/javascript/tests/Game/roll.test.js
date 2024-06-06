071ce864ae660c1046e48364f84a61b8d6c6298a
Brian Dady <bndady@gmail.com>
2024-06-06 13:58:30 -0500

    test Game.roll return value

--- bowling-game/javascript/tests/Game/roll.test.js
+++ bowling-game/javascript/tests/Game/roll.test.js
@@ -1,9 +1,12 @@
 describe("Game.roll should", () => {
   const Game = require("../../Game");
+  const game = new Game();
 
   test("be a function", () => {
-    const game = new Game();
-
     expect(game.roll).toBeInstanceOf(Function);
+  });
+
+  test("should not return anything", () => {
+    expect(game.roll()).not.toBeDefined();
   });
 });
