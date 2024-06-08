520131dcaece92732406262ae34a066044ee9655
Brian Dady <bndady@gmail.com>
2024-06-08 11:43:27 -0500

    test game.score calculates spare

--- bowling-game/javascript/Game.js
+++ bowling-game/javascript/Game.js
@@ -1,12 +1,34 @@
 module.exports = class Game {
+  #maxFrames = 10;
+  maxPins = 10;
   maxRolls = 20;
-  #score = 0;
+  maxStrikeRolls = 12;
+  #rolls = [];
 
   roll(pins) {
-    this.#score += pins;
+    this.#rolls.push(pins);
   }
 
   score() {
-    return this.#score;
+    let score = 0;
+    let rollsIndex = 0;
+
+    for (let frameIndex = 0; frameIndex < this.#maxFrames; frameIndex++) {
+      const nextRoll = this.#rolls[rollsIndex + 2] || 0;
+      const [firstRoll, secondRoll] = [
+        this.#rolls[rollsIndex] || 0,
+        this.#rolls[rollsIndex + 1] || 0,
+      ];
+
+      if (firstRoll + secondRoll === 10) {
+        score += firstRoll + secondRoll + nextRoll;
+      } else {
+        score += firstRoll + secondRoll;
+      }
+
+      rollsIndex += 2;
+    }
+
+    return score;
   }
 };
