ccccee1ffda4221c76637792fc8fa0a72d9f6e02
Brian Dady <bndady@gmail.com>
2024-06-09 21:15:57 -0500

    use "test" instead of "it" methods

--- string-calculator/javascript/StringCalculator.test.js
+++ string-calculator/javascript/StringCalculator.test.js
@@ -3,19 +3,19 @@ const StringCalculator = require("./StringCalculator");
 describe("StringCalculator", () => {
   const stringCalculator = new StringCalculator();
 
-  it("returns 0 when given an empty string", () => {
+  test("returns 0 when given an empty string", () => {
     expect(stringCalculator.add("")).toBe(0);
   });
 
-  it("returns the number when given a single number", () => {
+  test("returns the number when given a single number", () => {
     expect(stringCalculator.add("1")).toBe(1);
   });
 
-  it("returns the sum of two numbers", () => {
+  test("returns the sum of two numbers", () => {
     expect(stringCalculator.add("1,2")).toBe(3);
   });
 
-  it("returns the sum of an unknown number of numbers", () => {
+  test("returns the sum of an unknown number of numbers", () => {
     const randomNumber = Math.floor(Math.random() * 100 + 1);
 
     let numbers = [];
@@ -30,35 +30,35 @@ describe("StringCalculator", () => {
     expect(stringCalculator.add(numbersString)).toBe(sum);
   });
 
-  it("handles newline separators", () => {
+  test("handles newline separators", () => {
     expect(stringCalculator.add("1\n2")).toBe(3);
   });
 
-  it("handles newline and comma separators", () => {
+  test("handles newline and comma separators", () => {
     expect(stringCalculator.add("1\n2,3")).toBe(6);
   });
 
-  it("handles custom separator", () => {
+  test("handles custom separator", () => {
     expect(stringCalculator.add("//;\n1;2")).toBe(3);
   });
 
-  it("handles newline, comma, and custom separators", () => {
+  test("handles newline, comma, and custom separators", () => {
     expect(stringCalculator.add("//;\n1\n2,3;4")).toBe(10);
   });
 
-  it("throws an error if a negative number is given", () => {
+  test("throws an error if a negative number is given", () => {
     const expectedError = new RangeError("negatives not allowed");
 
     expect(() => stringCalculator.add("-1")).toThrow(expectedError);
   });
 
-  it("shows the negatives in the error when multiple negatives are given", () => {
+  test("shows the negatives in the error when multiple negatives are given", () => {
     const expectedError = new RangeError("negatives not allowed: -1, -1");
 
     expect(() => stringCalculator.add("-1,-1")).toThrow(expectedError);
   });
 
-  it("shows only the negatives in the error", () => {
+  test("shows only the negatives in the error", () => {
     const expectedError = new RangeError("negatives not allowed: -1, -3");
 
     expect(() => stringCalculator.add("-1,2,-3")).toThrow(expectedError);
